157fbc5963e34725672fdd66f01456d5b99343b7
Fernando Alberto Miranda Bonomi <fmirandabonomi@herrera.unt.edu.ar>
2023-05-17 17:29:45 -0300

    Eliminación de habilitación innecesaria de AFIO en configuración de pruebas

--- test/embedded/unity_config.c
+++ test/embedded/unity_config.c
@@ -4,7 +4,7 @@
 
 void unityOutputStart(unsigned long const baudrate){
     SystemCoreClockUpdate();
-    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_USART1EN | RCC_APB2ENR_AFIOEN;
+    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_USART1EN;
     GPIOA->CRH = (GPIOA->CRH & ~(0xF << GPIO_CRH_MODE9_Pos)) | (0b1011 << GPIO_CRH_MODE9_Pos);
     USART1->BRR = (SystemCoreClock+1) / baudrate;
     USART1->CR1 |= USART_CR1_TE | USART_CR1_UE;
