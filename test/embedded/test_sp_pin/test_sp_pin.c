b55ddacd60c7dce539f488add48087d1510d1885
Fernando Alberto Miranda Bonomi <fmirandabonomi@herrera.unt.edu.ar>
2023-05-29 18:05:20 -0300

    Simplificación de pruebas de pin GPIO

--- test/embedded/test_sp_pin/test_sp_pin.c
+++ test/embedded/test_sp_pin/test_sp_pin.c
@@ -2,193 +2,188 @@
 #include <unity.h>
 #include <stm32f1xx.h>
 
-static uint32_t rstCrh,rstCrl,rstOdr;
+/* CONSTANTES */
 
+/**
+ * @brief Registros del puerto GPIO donde se realizarán las pruebas
+ * 
+ */
+#define GPIO_PRUEBA GPIOB
+/**
+ * @brief Máscara del bit de habilitación de reloj en el registro 
+ * RCC->APB2ENR correspondiente a GPIO_PRUEBA
+ * 
+ */
+#define RCC_APB2ENR_GPIO_PRUEBA_ENABLE RCC_APB2ENR_IOPBEN
+/**
+ * @brief Máscara del bit de reset en el registro RCC->APB2ENR
+ * correspondiente a GPIO_PRUEBA
+ * 
+ */
+#define RCC_APB2RSTR_GPIO_PRUEBA_RESET RCC_APB2RSTR_IOPBRST
 
-typedef struct DeltaRegs{
-    uint32_t CRH;
-    uint32_t CRL;
-    uint32_t ODR;
-}DeltaRegs;
+/**
+ * @brief Número de pin del pin de prueba
+ * 
+ */
+#define PIN_PRUEBA 9
+/**
+ * @brief Handle del pin sobre el que se realizarán las pruebas
+ * de configuración, lectura y escritura
+ * 
+ */
+#define HPIN_PRUEBA SP_PB9
+/**
+ * @brief Registro de configuración correspondiente al pin
+ * PIN_PRUEBA
+ * 
+ */
+#define PIN_PRUEBA_CR (GPIO_PRUEBA->CRH)
+/**
+ * @brief Offset en el PIN_PRUEBA_CR donde se encuentra el
+ * bit menos significativo de los cuatro bits de 
+ * configuración del pin PIN_PRUEBA
+ * 
+ */
+#define PIN_PRUEBA_CR_Pos GPIO_CRH_MODE9_Pos
+/**
+ * @brief Máscara correspondiente PIN_PRUEBA en el registro
+ * de salida
+ * 
+ */
+#define ODR_PIN_PRUEBA_Msk (1<<PIN_PRUEBA)
+/**
+ * @brief Máscara correspondiente PIN_PRUEBA en el registro
+ * de entrada
+ * 
+ */
+#define IDR_PIN_PRUEBA_Msk (1<<PIN_PRUEBA)
+/**
+ * @brief Máscara correspondiente al bit de reset de
+ * PIN_PRUEBA en el registro BRR
+ * 
+ */
+#define BRR_PIN_PRUEBA_Msk (1<<PIN_PRUEBA)
+/**
+ * @brief Máscara correspondiente al bit de set de
+ * PIN_PRUEBA en el registro BSRR
+ * 
+ */
+#define BSRR_SET_PIN_PRUEBA_Msk (1<<PIN_PRUEBA)
+/**
+ * @brief Máscara correspondiente al bit de reset de
+ * PIN_PRUEBA en el registro BSRR
+ * 
+ */
+#define BSRR_RESET_PIN_PRUEBA_Msk (1<<(PIN_PRUEBA+16))
 
-static DeltaRegs getDeltaRegs(void){
-    return (DeltaRegs){
-        .CRH = rstCrh ^ GPIOB->CRH,
-        .CRL = rstCrl ^ GPIOB->CRL,
-        .ODR = rstOdr ^ GPIOB->ODR
-    };
+/* FUNCIONES DE UTILIDAD */
+static uint32_t GpioPrueba_reset(void){
+    RCC->APB2ENR |= RCC_APB2ENR_GPIO_PRUEBA_ENABLE;
+    RCC->APB2RSTR = RCC_APB2RSTR_GPIO_PRUEBA_RESET;
+    RCC->APB2RSTR = 0;
+    RCC->APB2ENR &= ~RCC_APB2ENR_GPIO_PRUEBA_ENABLE;
 }
-static void resetDeltaRegs(void){
-    rstCrh = GPIOB->CRH;
-    rstCrl = GPIOB->CRL;
-    rstOdr = GPIOB->ODR;    
+
+static uint32_t PinPrueba_getBitsMode(void){
+    return (PIN_PRUEBA_CR >> PIN_PRUEBA_CR_Pos) &0b0011;
 }
+static uint32_t PinPrueba_getBitsCnf(void){
+    return ((PIN_PRUEBA_CR >> (PIN_PRUEBA_CR_Pos)) & 0b1100)>>2;
+}
+
+static bool PinPrueba_qEsSalidaOpenDrain(void){
+    return PinPrueba_getBitsMode() !=0 && PinPrueba_getBitsCnf() == 0b01;
+}
+static bool PinPrueba_qEsSalidaPushPull(void){
+    return PinPrueba_getBitsMode() !=0 && PinPrueba_getBitsCnf() == 0b00;
+}
+static bool PinPrueba_qEsEntradaFlotante(void){
+    return PinPrueba_getBitsMode() == 0 && PinPrueba_getBitsCnf() == 0b01;
+}
+static bool PinPrueba_qEstadoOdr(void){
+    return GPIO_PRUEBA->ODR & ODR_PIN_PRUEBA_Msk;
+}
+static bool PinPrueba_qEsEntradaPullUp(void){
+    return    (PinPrueba_getBitsMode() == 0) 
+           && (PinPrueba_getBitsCnf()  == 0b10) 
+           && (PinPrueba_qEstadoOdr()  == 1);
+}
+static bool PinPrueba_qEsEntradaPullDown(void){
+    return    (PinPrueba_getBitsMode() == 0) 
+           && (PinPrueba_getBitsCnf()  == 0b10) 
+           && (PinPrueba_qEstadoOdr()  == 0);
+}
+
+
+/* setUp y tearDown */
 
 void setUp(void){
-    __disable_irq();
-    RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
-    RCC->APB2RSTR = RCC_APB2RSTR_IOPBRST;
-    RCC->APB2RSTR = 0;
-    resetDeltaRegs();
-    RCC->APB2ENR &= ~RCC_APB2ENR_IOPBEN;
-    __enable_irq();
+    GpioPrueba_reset();
 }
 
 void tearDown(void){
 }
 
-
-    enum{OFFSET = GPIO_CRH_MODE9_Pos, 
-         MASCARA_CRH = 0xFUL << OFFSET,
-         BIT_ODR = 9,
-         MASCARA_ODR = (1UL << BIT_ODR)};
+/* Definición de pruebas */
 
 static void test_config_salida(void){
-    enum{BITS_MODO_ALGUNO_HIGH = 0b0011,
-         BITS_MODO_LOW = 0b1100};
-    SP_Pin_setModo(SP_PB9,SP_PIN_ENTRADA);
-    resetDeltaRegs();
-    SP_Pin_setModo(SP_PB9,SP_PIN_SALIDA);
-    bool const GPIOB_ON = RCC->APB2ENR & RCC_APB2ENR_IOPBEN;
-    
-    DeltaRegs const DELTA = getDeltaRegs();
-
-    uint32_t const MODO = (GPIOB->CRH & MASCARA_CRH) >> OFFSET;
-
-    TEST_ASSERT_TRUE(GPIOB_ON);
-    TEST_ASSERT_BITS_LOW(~MASCARA_CRH,DELTA.CRH);
-    TEST_ASSERT_FALSE(DELTA.CRL);
-    TEST_ASSERT_FALSE(DELTA.ODR);
-    TEST_ASSERT_TRUE(MODO & BITS_MODO_ALGUNO_HIGH);
-    TEST_ASSERT_BITS_LOW(BITS_MODO_LOW, MODO);
+    TEST_ASSERT_TRUE(PinPrueba_qEsEntradaFlotante());
+    SP_Pin_setModo(HPIN_PRUEBA,SP_PIN_SALIDA);
+    TEST_ASSERT_TRUE(PinPrueba_qEsSalidaPushPull());
 }
+
 static void test_config_salidaOpenDrain(void){
-    enum{BITS_MODO_ALGUNO_HIGH = 0b0011,
-         BITS_CFG = 0b1100,
-         BITS_CFG_ESPERADO = 0b0100};
-    SP_Pin_setModo(SP_PB9,SP_PIN_ENTRADA);
-    resetDeltaRegs();
-    SP_Pin_setModo(SP_PB9,SP_PIN_SALIDA_OPEN_DRAIN);
-    bool const GPIOB_ON = RCC->APB2ENR & RCC_APB2ENR_IOPBEN;
-    
-    DeltaRegs const DELTA = getDeltaRegs();
-
-    uint32_t const MODO = (GPIOB->CRH & MASCARA_CRH) >> OFFSET;
-
-    TEST_ASSERT_TRUE(GPIOB_ON);
-    TEST_ASSERT_BITS_LOW(~MASCARA_CRH,DELTA.CRH);
-    TEST_ASSERT_FALSE(DELTA.CRL);
-    TEST_ASSERT_FALSE(DELTA.ODR);
-    TEST_ASSERT_TRUE(MODO & BITS_MODO_ALGUNO_HIGH);
-    TEST_ASSERT_BITS(BITS_CFG, BITS_CFG_ESPERADO,MODO);
+    TEST_ASSERT_TRUE(PinPrueba_qEsEntradaFlotante());
+    SP_Pin_setModo(HPIN_PRUEBA,SP_PIN_SALIDA_OPEN_DRAIN);
+    TEST_ASSERT_TRUE(PinPrueba_qEsSalidaOpenDrain());
 }
 static void test_config_entrada(void){
-    enum{MODO_ESPERADO = 0b0100};
-    SP_Pin_setModo(SP_PB9,SP_PIN_SALIDA);
-    resetDeltaRegs();
-    SP_Pin_setModo(SP_PB9,SP_PIN_ENTRADA);
-    bool const GPIOB_ON = RCC->APB2ENR & RCC_APB2ENR_IOPBEN;
-
-    DeltaRegs const DELTA = getDeltaRegs();
-
-    uint32_t const MODO = (GPIOB->CRH & MASCARA_CRH) >> OFFSET;
-
-    TEST_ASSERT_TRUE(GPIOB_ON);
-    TEST_ASSERT_BITS_LOW(~MASCARA_CRH,DELTA.CRH);
-    TEST_ASSERT_FALSE(DELTA.CRL);
-    TEST_ASSERT_FALSE(DELTA.ODR);
-    TEST_ASSERT_EQUAL_HEX8(MODO_ESPERADO,MODO);
+    SP_Pin_setModo(HPIN_PRUEBA,SP_PIN_SALIDA); // Verificado en otra prueba
+    SP_Pin_setModo(HPIN_PRUEBA,SP_PIN_ENTRADA);
+    TEST_ASSERT_TRUE(PinPrueba_qEsEntradaFlotante());
 }
 
 static void test_config_entradaPullUp(void){
-    enum{MODO_ESPERADO = 0b1000};
-    SP_Pin_setModo(SP_PB9,SP_PIN_ENTRADA);
-    resetDeltaRegs();
-    SP_Pin_setModo(SP_PB9,SP_PIN_ENTRADA_PULLUP);
-    bool const GPIOB_ON = RCC->APB2ENR & RCC_APB2ENR_IOPBEN;
-
-    DeltaRegs const DELTA = getDeltaRegs();
-    uint32_t const ODR = GPIOB->ODR;
-
-    uint32_t const MODO = (GPIOB->CRH & MASCARA_CRH) >> OFFSET;
-    
-    TEST_ASSERT_TRUE(GPIOB_ON);
-    TEST_ASSERT_BITS_LOW(~MASCARA_CRH,DELTA.CRH);
-    TEST_ASSERT_FALSE(DELTA.CRL);
-    TEST_ASSERT_EQUAL_HEX8(MODO_ESPERADO,MODO);
-    TEST_ASSERT_BITS_LOW(~MASCARA_ODR,DELTA.ODR);
-    TEST_ASSERT_BIT_HIGH(BIT_ODR,ODR);
+    SP_Pin_setModo(HPIN_PRUEBA,SP_PIN_SALIDA); // Verificado en otra prueba
+    SP_Pin_setModo(HPIN_PRUEBA,SP_PIN_ENTRADA_PULLUP);
+    TEST_ASSERT_TRUE(PinPrueba_qEsEntradaPullUp());
 }
 
 static void test_SP_Pin_setModo__SP_PIN_ENTRADA_PULLDN__DEBE_configurar_pin_como_ENTRADA_con_PULL_DOWN_INTERNO(void){
-    enum{MODO_ESPERADO = 0b1000};
-    SP_Pin_setModo(SP_PB9,SP_PIN_ENTRADA_PULLDN);
-    bool const GPIOB_ON = RCC->APB2ENR & RCC_APB2ENR_IOPBEN;
-
-    DeltaRegs const DELTA = getDeltaRegs();
-    uint32_t const ODR = GPIOB->ODR;
-
-    uint32_t const MODO = (GPIOB->CRH & MASCARA_CRH) >> OFFSET;
-    TEST_ASSERT_TRUE(GPIOB_ON);
-    TEST_ASSERT_BITS_LOW(~MASCARA_CRH,DELTA.CRH);
-    TEST_ASSERT_FALSE(DELTA.CRL);
-    TEST_ASSERT_EQUAL_HEX8(MODO_ESPERADO,MODO);
-    TEST_ASSERT_BITS_LOW(~MASCARA_ODR,DELTA.ODR);
-    TEST_ASSERT_BIT_LOW(BIT_ODR,ODR);
+    SP_Pin_setModo(HPIN_PRUEBA,SP_PIN_SALIDA); // Verificado en otra prueba
+    SP_Pin_setModo(HPIN_PRUEBA,SP_PIN_ENTRADA_PULLDN);
+    TEST_ASSERT_TRUE(PinPrueba_qEsEntradaPullDown());
 }
 
 static void test_SP_Pin_write__TRUE__DEBE_establecer_pin_en_ALTO(void){
-    SP_Pin_setModo(SP_PB9,SP_PIN_SALIDA_OPEN_DRAIN);
-    resetDeltaRegs();
-    SP_Pin_write(SP_PB9,1);
-    DeltaRegs DELTA = getDeltaRegs();
-    uint32_t ODR = GPIOB->ODR;
-    TEST_ASSERT_FALSE(DELTA.CRH);
-    TEST_ASSERT_FALSE(DELTA.CRL);
-    TEST_ASSERT_BITS_LOW(~MASCARA_ODR,DELTA.ODR);
-    TEST_ASSERT_BIT_HIGH(BIT_ODR,DELTA.ODR);
-    TEST_ASSERT_BIT_HIGH(BIT_ODR,ODR);
+    SP_Pin_setModo(HPIN_PRUEBA,SP_PIN_SALIDA); // Verificado en otra prueba
+    TEST_ASSERT_EQUAL(0,PinPrueba_qEstadoOdr());
+    SP_Pin_write(HPIN_PRUEBA,1);
+    TEST_ASSERT_EQUAL(1,PinPrueba_qEstadoOdr());
 }
 
 static void test_SP_Pin_write__FALSE__DEBE_establecer_pin_en_BAJO(void){
-    SP_Pin_setModo(SP_PB9,SP_PIN_SALIDA_OPEN_DRAIN);
-    SP_Pin_write(SP_PB9,1);
-    resetDeltaRegs();
-    SP_Pin_write(SP_PB9,0);
-    DeltaRegs DELTA = getDeltaRegs();
-    uint32_t ODR = GPIOB->ODR;
-    TEST_ASSERT_FALSE(DELTA.CRH);
-    TEST_ASSERT_FALSE(DELTA.CRL);
-    TEST_ASSERT_BITS_LOW(~MASCARA_ODR,DELTA.ODR);
-    TEST_ASSERT_BIT_LOW(BIT_ODR,ODR);
+    SP_Pin_setModo(HPIN_PRUEBA,SP_PIN_SALIDA); // Verificado en otra prueba
+    SP_Pin_write(HPIN_PRUEBA,1);               // Verificado en otra prueba
+    SP_Pin_write(HPIN_PRUEBA,0);
+    TEST_ASSERT_EQUAL(0,PinPrueba_qEstadoOdr());
 }
 
 static void test_SP_Pin_read__DEBE_retornar_TRUE_si_la_entrada_es_ALTA(void){
-    SP_Pin_setModo(SP_PB9,SP_PIN_SALIDA);
-    SP_Pin_write(SP_PB9,1);
-    SP_Pin_setModo(SP_PB9,SP_PIN_ENTRADA);
-    SP_Pin_write(SP_PB9,0);
-    resetDeltaRegs();
-    bool const valor_1 = SP_Pin_read(SP_PB9);
-    DeltaRegs DELTA = getDeltaRegs();
-    TEST_ASSERT_FALSE(DELTA.CRH);
-    TEST_ASSERT_FALSE(DELTA.CRL);
-    TEST_ASSERT_FALSE(DELTA.ODR);
-    TEST_ASSERT_TRUE(valor_1);
+    SP_Pin_setModo(HPIN_PRUEBA,SP_PIN_SALIDA);      // Verificado en otra prueba
+    SP_Pin_write(HPIN_PRUEBA,1);                    // Verificado en otra prueba
+    SP_Pin_setModo(HPIN_PRUEBA,SP_PIN_ENTRADA);     // Verificado en otra prueba
+    SP_Pin_write(HPIN_PRUEBA,0);                    // Verificado en otra prueba
+    TEST_ASSERT_EQUAL(1,SP_Pin_read(HPIN_PRUEBA));
 }
 
 static void test_SP_Pin_read__DEBE_retornar_FALSE_si_la_entrada_es_BAJA(void){
-    SP_Pin_setModo(SP_PB9,SP_PIN_SALIDA);
-    SP_Pin_write(SP_PB9,0);
-    SP_Pin_setModo(SP_PB9,SP_PIN_ENTRADA);
-    SP_Pin_write(SP_PB9,1);
-    resetDeltaRegs();
-    bool const valor_0 = SP_Pin_read(SP_PB9);
-    DeltaRegs DELTA = getDeltaRegs();
-    TEST_ASSERT_FALSE(DELTA.CRH);
-    TEST_ASSERT_FALSE(DELTA.CRL);
-    TEST_ASSERT_FALSE(DELTA.ODR);
-    TEST_ASSERT_FALSE(valor_0);
+    SP_Pin_setModo(HPIN_PRUEBA,SP_PIN_SALIDA);      // Verificado en otra prueba
+    SP_Pin_write(HPIN_PRUEBA,0);                    // Verificado en otra prueba
+    SP_Pin_setModo(HPIN_PRUEBA,SP_PIN_ENTRADA);     // Verificado en otra prueba
+    SP_Pin_write(HPIN_PRUEBA,1);                    // Verificado en otra prueba
+    TEST_ASSERT_EQUAL(0,SP_Pin_read(HPIN_PRUEBA));
 }
 
 static void test_los_pines_asociados_a_JTAG__DEBEN_ser_desasociados_al_configurar(void){
@@ -208,6 +203,9 @@ static void test_los_pines_asociados_a_JTAG__DEBEN_ser_desasociados_al_configura
         TEST_ASSERT_TRUE(valor_1);
     }
 }
+
+
+/* Ejecución de pruebas */
 
 int main(void){
     SP_init();
